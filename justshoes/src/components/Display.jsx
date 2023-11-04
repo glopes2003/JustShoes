0af476a602578ed433bd6a8ce731df572b284fab
GLopes <gicaalopes2003@gmail.com>
2023-11-03 21:26:12 -0300

    putting shoes display in a component

---
+++ justshoes/src/components/Display.jsx
@@ -0,0 +1,77 @@
+function Display() {
+  return (
+    <>
+      <section>
+        <article>
+          <div>
+            <img src="../src/assets/heart.png" alt="favorite"></img>
+          </div>
+          <img src="../src/assets/1.png" alt="nike-air-force-1"></img>
+          <p>Tênis Nike Air Force 1 Shadow Casual</p>
+          <p> R$ 250,00</p>
+        </article>
+
+        <article>
+          <div>
+            <img src="../src/assets/heart.png" alt="favorite"></img>
+          </div>
+          <img
+            src="../src/assets/2.png"
+            alt="nike-air-jordan-1-low-casual"
+          ></img>
+          <p>Tênis Air Jordan 1 Low Casual </p>
+          <p>R$ 320,99</p>
+        </article>
+
+        <article>
+          <div>
+            <img src="../src/assets/heart.png" alt="favorite"></img>
+          </div>
+          <img
+            src="../src/assets/3.png"
+            alt="nike-court-legacy-lift-casual"
+          ></img>
+          <p>Tênis Nike Court Legacy Lift Casual</p>
+          <p>R$ 249,99</p>
+        </article>
+
+        <article>
+          <div>
+            <img src="../src/assets/heart.png" alt="favorite"></img>
+          </div>
+          <img
+            src="../src/assets/4.png"
+            alt="nike-court-vision-low-casual"
+          ></img>
+          <p>Tênis Nike Court Vision Low Casual</p>
+          <p>R$ 200,00</p>
+        </article>
+
+        <article>
+          <div>
+            <img src="../src/assets/heart.png" alt="favorite"></img>
+          </div>
+          <img
+            src="../src/assets/5.png"
+            alt="nike-air-jordan-1-mid-casual"
+          ></img>
+          <p>Tênis Air Jordan 1 Mid Casual</p>
+          <p>R$ 419,99</p>
+        </article>
+
+        <article>
+          <div>
+            <img src="../src/assets/heart.png" alt="favorite"></img>
+          </div>
+          <img
+            src="../src/assets/6.png"
+            alt="nike-air-force-1-'07-casual"
+          ></img>
+          <p>Tênis Nike Air Force 1 &rsquo;07 Casual</p>
+          <p> R$220,00</p>
+        </article>
+      </section>
+    </>
+  );
+}
+export default Display;
