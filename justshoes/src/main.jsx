1c4e39ac0c4763c6732618d9e5b752e5156b4ccc
GLopes <gicaalopes2003@gmail.com>
2023-11-03 21:26:47 -0300

    rendering brand page

--- justshoes/src/main.jsx
+++ justshoes/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Footer from './footer'
+import Brand from './brand'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Footer/>
+    <Brand/>
   </React.StrictMode>,
 )
