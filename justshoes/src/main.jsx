cab78e586e16650e0cb760e65dae68e0a236e580
GLopes <gicaalopes2003@gmail.com>
2023-11-04 15:15:08 -0300

    rendering navbar on all pages

--- justshoes/src/main.jsx
+++ justshoes/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Brand from './pages/Brand'
+import Navbar from './components/Navbar'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Navbar/>
     <Brand/>
   </React.StrictMode>,
 )
