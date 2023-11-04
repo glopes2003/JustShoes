8505863723248cfca2cfb334478a778d23350299
GLopes <gicaalopes2003@gmail.com>
2023-11-04 15:06:01 -0300

    fixing import

--- justshoes/src/main.jsx
+++ justshoes/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Brand from './brand'
+import Brand from './pages/Brand'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
