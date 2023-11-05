4936b3adcc1e9dff7ec34ac9fa4133ffccb22d9d
GLopes <gicaalopes2003@gmail.com>
2023-11-04 23:32:10 -0300

    rendering shoe page

--- justshoes/src/main.jsx
+++ justshoes/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Navbar from './Navbar'
+import Shoe from './pages/shoe'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navbar/>
+    <Shoe/>
   </React.StrictMode>,
 )
