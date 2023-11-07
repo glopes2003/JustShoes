685a1696bd2d59ee95c23baad6abe369a48a6d02
GLopes <gicaalopes2003@gmail.com>
2023-11-07 14:52:04 -0300

    adding some routes

--- justshoes/src/main.jsx
+++ justshoes/src/main.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Brand from './pages/Brand'
-import Navbar from './components/Navbar'
-import Shoe from './pages/shoe'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Brand from "./pages/Brand";
+import Navbar from "./components/Navbar";
+import Shoe from "./pages/shoe";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Navbar/>
-    <Shoe/>
-    <Brand/>
-  </React.StrictMode>,
-)
+    <Navbar />
+    <Router>
+      <Routes>
+        <Route path="/brand" element={<Brand />} />
+        <Route path="/shoe" element={<Shoe />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
