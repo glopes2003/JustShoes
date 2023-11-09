704c7f73a37e8a4f4d1289622dabfab9abd4a75c
GLopes <gicaalopes2003@gmail.com>
2023-11-08 22:30:27 -0300

    continuing code refactoring

--- justshoes/src/components/Display.jsx
+++ justshoes/src/components/Display.jsx
@@ -1,96 +1,123 @@
-import "../style/display.css";
+import "../style/index.css";
 
 function Display() {
   return (
     <>
-      <section className="wrapper">
-        <div className="display">
-          <article>
-            <div className="container">
-              <img src="../src/assets/heart.png" alt="favorite"></img>
+      <section className="d-flex justify-content-center align-items-center">
+        <div className="display mb-5">
+          <div>
+            <div className="rounded pic">
+              <img
+                className="float-end m-1"
+                src="../src/assets/heart.png"
+                alt="favorite"
+              ></img>
               <img src="../src/assets/1.png" alt="nike-air-force-1"></img>
             </div>
-            <div className="info">
-              <p>Tênis Nike Air Force 1 Shadow Casual</p>
-              <p> R$ 250,00</p>
+            <div className="details fs-5">
+              <p className="fw-medium">Tênis Nike Air Force 1 Shadow Casual</p>
+              <p className="fw-semibold"> R$ 250,00</p>
             </div>
-          </article>
+          </div>
 
           <article>
-            <div className="container">
-              <img src="../src/assets/heart.png" alt="favorite"></img>
+            <div className="rounded pic">
+              <img
+                className="float-end m-1"
+                src="../src/assets/heart.png"
+                alt="favorite"
+              ></img>
               <img
                 src="../src/assets/2.png"
                 alt="nike-air-jordan-1-low-casual"
               ></img>
             </div>
 
-            <div className="info">
-              <p>Tênis Air Jordan 1 Low Casual </p>
-              <p>R$ 320,99</p>
+            <div className="details fs-5">
+              <p className="fw-medium">Tênis Air Jordan 1 Low Casual </p>
+              <p className="fw-semibold">R$ 320,99</p>
             </div>
           </article>
 
           <article>
-            <div className="container">
-              <img src="../src/assets/heart.png" alt="favorite"></img>
+            <div className="rounded pic">
+              <img
+                className="float-end m-1"
+                src="../src/assets/heart.png"
+                alt="favorite"
+              ></img>
               <img
                 src="../src/assets/3.png"
                 alt="nike-court-legacy-lift-casual"
               ></img>
             </div>
-            <div className="info">
-              <p>Tênis Nike Court Legacy Lift Casual</p>
-              <p>R$ 249,99</p>
+            <div className="details fs-5">
+              <p className="fw-medium">Tênis Nike Court Legacy Lift Casual</p>
+              <p className="fw-semibold">R$ 249,99</p>
             </div>
           </article>
 
           <article>
-            <div className="container">
-              <img src="../src/assets/heart.png" alt="favorite"></img>
+            <div className="rounded pic">
+              <img
+                className="float-end m-1"
+                src="../src/assets/heart.png"
+                alt="favorite"
+              ></img>
               <img
                 src="../src/assets/4.png"
                 alt="nike-court-vision-low-casual"
               ></img>
             </div>
-            <div className="info">
-              <p>Tênis Nike Court Vision Low Casual</p>
-              <p>R$ 200,00</p>
+            <div className="details fs-5">
+              <p className="fw-medium">Tênis Nike Court Vision Low Casual</p>
+              <p className="fw-semibold">R$ 200,00</p>
             </div>
           </article>
 
           <article>
-            <div className="container">
-              <img src="../src/assets/heart.png" alt="favorite"></img>
+            <div className="rounded pic">
+              <img
+                className="float-end m-1"
+                src="../src/assets/heart.png"
+                alt="favorite"
+              ></img>
               <img
                 src="../src/assets/5.png"
                 alt="nike-air-jordan-1-mid-casual"
               ></img>
             </div>
-            <div className="info">
-              <p>Tênis Air Jordan 1 Mid Casual</p>
-              <p>R$ 419,99</p>
+            <div className="details fs-5">
+              <p className="fw-medium">Tênis Air Jordan 1 Mid Casual</p>
+              <p className="fw-semibold">R$ 419,99</p>
             </div>
           </article>
 
-          <article>
-            <div className="container">
-              <img src="../src/assets/heart.png" alt="favorite"></img>
+          <div>
+            <div className=" rounded pic">
               <img
+                className="float-end m-1"
+                src="../src/assets/heart.png"
+                alt="favorite"
+              ></img>
+              <img
+              className="divv"
                 src="../src/assets/6.png"
                 alt="nike-air-force-1-'07-casual"
               ></img>
             </div>
-            <div className="info">
-              <p>Tênis Nike Air Force 1 &rsquo;07 Casual</p>
-              <p> R$220,00</p>
+            <div className="details fs-5">
+              <p className="fw-medium">
+                Tênis Nike Air Force 1 &rsquo;07 Casual
+              </p>
+              <p className="fw-semibold"> R$220,00</p>
             </div>
-          </article>
+          </div>
         </div>
       </section>
-      <div className="button">
-        <button>
-          <img src="../src/assets/arrow-down.svg" alt="load-more-shoes"></img>
+      <div className="d-flex justify-content-center align-items-center ">
+        <button className="p-4 bg-transparent mt-5 mb-5 load fs-4 fw-semibold text-center">
+          <img className="float-end " src="../src/assets/arrow-down.svg" alt="load-more-shoes"></img>
           CARREGAR MAIS
         </button>
       </div>
