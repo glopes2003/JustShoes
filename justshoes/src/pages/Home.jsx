b4442dea92e9d69d3c01f1fd1c79c94cfa11ad1d
GLopes <gicaalopes2003@gmail.com>
2023-11-14 16:22:52 -0300

    continuing page styling with the help of bootstrap

--- justshoes/src/pages/Home.jsx
+++ justshoes/src/pages/Home.jsx
@@ -3,29 +3,31 @@ import "../style/home.css";
 function Home() {
   return (
     <>
-      <section className="d-flex justify-content-between align-items-center ps-5  banner-sale">
-        <div className="">
+      <section className="d-flex justify-content-between align-items-center ps-5 banner-sale">
+        <div>
           <h1 className="fw-bold banner-title">REAGE, CALÇA UM TÊNIS!</h1>
           <p className="fw-medium my-5 banner-subtitle">
             Sapatos são mais que um artigo de uso, são um estilo de vida!
           </p>
-          <button className="rounded-pill p-3 w-20 fs-6 fw-medium">
+          <button className="rounded-pill fw-medium buy-button">
             Compre Agora
             <img
-              className="d-inline-block position-absolute "
+              className="d-inline-block position-absolute ms-3 "
               src="../src/assets/arrow-up.svg"
             ></img>
           </button>
         </div>
         <img
-          className="ms-5"
+          className="ms-5 overflow-hidden"
           src="../src/assets/banner-sale.svg"
           alt="banner-sale"
         ></img>
       </section>
 
-      <section>
-        <p>Escolha a marca que mais combina com seu estilo!</p>
+      <section className="brand-options my-5 p-5">
+        <p className="d-flex justify-content-between align-items-center fs-2">
+          Escolha a marca que mais combina com seu estilo!
+        </p>
         <img src="../src/assets/adidas.svg"></img>
         <img src="../src/assets/converse.svg"></img>
         <img src="../src/assets/nike.svg"></img>
