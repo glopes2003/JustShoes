3c74495311201a9a0184c4ffd550a63ba56594b4
GLopes <gicaalopes2003@gmail.com>
2023-11-13 21:52:33 -0300

    structuring home page

--- justshoes/src/pages/Home.jsx
+++ justshoes/src/pages/Home.jsx
@@ -3,11 +3,25 @@ import "../style/home.css";
 function Home() {
   return (
     <>
-     <section>
-        
+      <section>
+        <div>
+          <h1>REAGE, CALÇA UM TÊNIS!</h1>
+          <p>Sapatos são mais que um artigo de uso, são um estilo de vida</p>
+          <button>
+            <img src="../src/assets/arrow-up.svg"></img>Compre Agora
+          </button>
+          <img src="../src/assets/banner-sale.svg" alt="banner-sale"></img>
+        </div>
+      </section>
 
-     </section>
-      
+      <section>
+        <p>Escolha a marca que mais combina com seu estilo!</p>
+        <img src="../src/assets/adidas.svg"></img>
+        <img src="../src/assets/converse.svg"></img>
+        <img src="../src/assets/nike.svg"></img>
+        <img src="../src/assets/puma.svg"></img>
+        <img src="../src/assets/vans.svg"></img>
+      </section>
     </>
   );
 }
