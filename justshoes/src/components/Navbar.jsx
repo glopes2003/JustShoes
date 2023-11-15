b4f88041bcacfe4732fae0e8404a999b06baddf2
GLopes <gicaalopes2003@gmail.com>
2023-11-14 22:31:06 -0300

    refactoring navbar code

--- justshoes/src/components/Navbar.jsx
+++ justshoes/src/components/Navbar.jsx
@@ -1,35 +1,110 @@
 import logo from "../assets/logo.svg";
-import searchIcon from "../assets/search-icon.svg";
+// import searchIcon from "../assets/search-icon.svg";
 import profile from "../assets/profile.png";
 import favorites from "../assets/heart.png";
 import cart from "../assets/shopping bag.png";
 
-import styles from "../style/navbar.module.css";
-
 const Navbar = () => {
   return (
     <>
-      <nav className={styles.navbar}>
-        <img className={styles.img_logo} src={logo} alt="Logo" />
-        <ul>
-          <li>OUTLET</li>
-          <li>Adidas</li>
-          <li>Converse</li>
-          <li>Nike</li>
-          <li>Puma</li>
-          <li>Vans</li>
-        </ul>
-        <div className={styles.search_bar}>
-          <img src={searchIcon} alt="search" />
-          <span>Buscar</span>
+      <section className="header p-5">
+        <div>
+          <nav className="navbar navbar-expand-lg pl-0 pr-0">
+            <a className="navbar-brand" href="#">
+              <img src={logo} alt="Logo"></img>
+            </a>
+
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNavDropDown">
+              <ul className="navbar-nav fs-5 fw-medium mx-auto">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    OUTLET
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Adidas
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Converse
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Nike
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Puma
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Vans
+                  </a>
+                </li>
+              </ul>
+
+              <span className=" d-flex flex-row justify-content-center align-items-center">
+                <a className="btn btn-white btn-icon" href="#">
+                  <span className="icon">
+                    <img src={profile} alt="profile" />
+                  </span>
+                </a>
+                <a className="btn btn-white btn-icon" href="#">
+                  <span className="icon">
+                    <img src={favorites} alt="favorites" />
+                  </span>
+                </a>
+                <a className="btn btn-white btn-icon" href="#">
+                  <span className="icon cart-img">
+                    <img src={cart} alt="cart" />
+                  </span>
+                </a>
+                <a className="btn btn-white btn-icon" href="#">
+                  <span className="icon fs-4">
+                    0
+                  </span>
+                </a>
+              </span>
+            </div>
+          </nav>
         </div>
-        <div className={styles.container_icons}>
-          <img src={profile} alt="profile" />
-          <img src={favorites} alt="favorites" />
-          <img src={cart} alt="cart" />
-          <span>0</span>
-        </div>
-      </nav>
+      </section>
+
+      {/* 
+        <form className="d-flex w-25" role="search">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Buscar"
+            aria-label="Search"
+          />
+          <span className="d-inline-block position-absolute ">
+            <button
+              className="btn "
+              type="submit"
+            >
+              <img src={searchIcon} alt="search" />
+            </button>
+          </span>
+        </form>
+        */}
     </>
   );
 };
