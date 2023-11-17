bf2096357c2b84a808b043e6b5bed10902db6883
GLopes <gicaalopes2003@gmail.com>
2023-11-16 23:30:14 -0300

    adding cart count update to navbar

--- justshoes/src/main.jsx
+++ justshoes/src/main.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Brand from "./pages/Brand";
 import Shoe from "./pages/Shoe";
-import Home from './pages/Home';
+import Home from "./pages/Home";
 
-import './style/index.css';
+import "./style/index.css";
 
+const Main = () => {
+  const [cartCount, setCartCount] = useState(0);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Navbar />
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/brand" element={<Brand />} />
-        <Route path="/shoe" element={<Shoe />} />
-      </Routes>
-    </Router>
-    <Footer />
-  </React.StrictMode>
-);
-   
-    
-  
+  const handleAddToCart = () => {
+    setCartCount(cartCount + 1);
+  };
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Navbar cartCount={cartCount} />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/brand" element={<Brand />} />
+          <Route
+            path="/shoe"
+            element={<Shoe handleAddToCart={handleAddToCart} />}
+          />
+        </Routes>
+      </Router>
+      <Footer />
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Main />);
